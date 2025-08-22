e061e401181d727107e88c7a69af8f0969c25796
ncimenian12345 <45405397+ncimenian12345@users.noreply.github.com>
2025-08-22 12:25:11 -0700

    Update tailwind-config.js

--- tailwind-config.js
+++ tailwind-config.js
@@ -1,12 +1,8 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
-content: [
-"./index.html",
-"./src/**/*.{js,ts,jsx,tsx}",
-],
-theme: {
-extend: {},
-},
-plugins: [],
-};
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  // ...
+}
